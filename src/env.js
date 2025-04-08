d7df4c737864e06f123bed2c9b699b66dc6d9938
Mohammed Patel <mohammed.patel10@dcmail.ca>
2025-04-08 13:46:57 -0400

    deleted discord connection

--- src/env.js
+++ src/env.js
@@ -6,18 +6,7 @@ export const env = createEnv({
 	 * Specify your server-side environment variables schema here. This way you can ensure the app
 	 * isn't built with invalid env vars.
 	 */
-	server: {
-		AUTH_SECRET:
-			process.env.NODE_ENV === "production"
-				? z.string()
-				: z.string().optional(),
-		AUTH_DISCORD_ID: z.string(),
-		AUTH_DISCORD_SECRET: z.string(),
-		DATABASE_URL: z.string().url(),
-		NODE_ENV: z
-			.enum(["development", "test", "production"])
-			.default("development"),
-	},
+
 
 	/**
 	 * Specify your client-side environment variables schema here. This way you can ensure the app
@@ -34,8 +23,6 @@ export const env = createEnv({
 	 */
 	runtimeEnv: {
 		AUTH_SECRET: process.env.AUTH_SECRET,
-		AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
-		AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
 		DATABASE_URL: process.env.DATABASE_URL,
 		NODE_ENV: process.env.NODE_ENV,
 	},
